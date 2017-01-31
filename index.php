bc7628349d2b0f31d4eb575481987cd5025512eb
jezjsa <jez@nineten.co.uk>
2017-01-31 13:41:26 +0000

    added css, logo and index

---
+++ index.php
@@ -0,0 +1,209 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Candy Digital | The CRM Database Design Experts</title>
+<meta name="viewport" content="width=device-width, user-scalable=no">
+<link rel="stylesheet" type="text/css" href="/gridwow.css">
+<link href="https://fonts.googleapis.com/css?family=Roboto:400,700,900" rel="stylesheet">
+
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+
+</head>
+
+<body>
+<div class="container" id="top">
+<div class="row">
+<div class="c12 ban">
+<img src="/guy.png" />
+<p class="logo">Candy</p>
+<h1><b>Candy Software</b> | The CRM Database Design Experts</h1>
+
+</div>
+</div>
+
+<div class="pad">
+<div class="services">
+<div class="row">
+<div class="c12">
+<div class="c10 last">
+<p class="intro">Here at <b>Candy</b>, we design and develop bespoke Web Based CRM Systems.</p>
+<p>A bespoke web based crm is a system specifically designed for your business, that runs in a web browser, meaning your data can be accessed securely via a desktop computer, tablet device or smartphone from any location.</p> 
+<p>A bespoke CRM system can hold all types of data, including lead generation, jobs, tasks, clients, notes, quotes, invoicing, reminders, sales forecasting, performance tracking and much much more. It can pull data to and from Sage, or MailChimp, really whatever your business requires, as these are systems specifically created for your business. </p>
+
+
+
+</div>
+
+<p class="intouch">If you have a project in mind, <a href="#contact">please get in touch</a>.</p>
+</div>
+</div>
+
+
+
+</div>
+
+
+<div class="services">
+<div class="row">
+<div class="c12">
+<h2>Services</h2>
+<p class="big">We design and build a range of CRM soltions, here are our main areas of expertise.</p>
+</div>
+</div>
+
+
+<div class="row">
+<div class="c10 last">
+<div class="c4">
+
+<h3>Database Design</h3>
+<p>Content to go here about CRM design</p>
+</div>
+
+<div class="c4">
+
+<h3>CRM Systems</h3>
+<p>Content to go here about CRM design</p>
+</div>
+
+<div class="c4 last">
+
+<h3>App Development</h3>
+<p>Content to go here about CRM design</p>
+</div>
+</div>
+
+</div>
+</div>
+
+
+<div class="projects">
+<div class="row">
+<div class="c12">
+<h2>Projects</h2>
+<p class="big">Current projects with clients based internationally.</p>
+</div>
+</div>
+
+
+<div class="row">
+<div class="c10 last">
+
+<div class="c3">
+<img src="/1.png" />
+<h3>Surveyor<br> CRM Design</h3>
+<p>The design, development and ongoing support for a bespoke CRM system for Los Angeles based surveying company.</p>
+</div>
+
+<div class="c3">
+<img src="/1.png" />
+<h3>Property<br> CRM Design</h3>
+<p>Design and development of bespoke property management CRM system for Portugal based property consultancy.</p>
+</div>
+
+<div class="c3">
+<img src="/1.png" />
+<h3>Bitcoin<br> CRM Design</h3>
+<p>Design and development of bespoke cryptocurrency CRM system for London based consultancy.</p>
+</div>
+
+<div class="c3 last">
+<img src="/1.png" />
+<h3>Course Training<br> CRM Design</h3>
+<p>Design and development of bespoke website and CRM system for Yorkshire based course training providers.</p>
+</div>
+
+
+</div>
+
+</div>
+</div>
+
+<div class="comments">
+<div class="row">
+<div class="c12">
+<h2>Testimonials</h2>
+<div class="text">
+<p>Rotating comments from clients to go here.</p>
+</div></div>
+</div>
+
+
+<div class="row">
+<div class="c10">
+
+</div>
+
+</div>
+</div>
+
+<div class="contact">
+<div class="row">
+<div class="c10 last" id="about">
+<h2>About us</h2>
+<p class="big">We're a small nimble team of usability experts, designers and programmers based in Doncaster, South Yorkshire. We set the business up in 2013, though as a team, we all have over 15 years experience in our fields.</p> <p class="big">Rest assured, you are in safe hands.</p>
+</div>
+</div>
+
+
+<div class="row">
+<div class="c10">
+
+</div>
+
+</div>
+</div>
+
+<div class="contact">
+<div class="row">
+<div class="c12" id="contact">
+<h2>Contact us</h2>
+<p class="big">If you have a project in mind, send us your details and we'll be in touch asap.</p>
+</div>
+</div>
+
+
+<div class="row">
+<div class="c10">
+
+</div>
+
+</div>
+</div>
+<div><p><a href="#top">Back to top</a></p></div>
+</div>
+<div class="footer">
+<div class="row">
+<div class="c12">
+
+<p>&copy; Candy Software Limited 2016<br> <br>
+A Doncaster based CRM Development and Design Company</p>
+</div>
+</div>
+
+
+<div class="row">
+<div class="c10">
+
+</div>
+
+</div>
+</div>
+</div>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
+<script>$('a[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            $('html, body').animate({ scrollTop: target.offset().top }, 1000);
+            return false;
+        }
+    }
+});
+//# sourceURL=pen.js
+</script>
+</body>
+</html>
